e5d3f7b6e14adc4c3e1440ee8c7380818e004240
kingranzz <156513688+kingranzz@users.noreply.github.com>
2025-03-16 04:44:44 +0800

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,10 @@
+const global = {
+    owner: [7316016470], // ganti jadi id mu
+    botToken: "7853210586:AAGebXce-RAHVMTWtCjcO3kkh_OvSMnTKzs", //isi make token bot mu
+}
+
+module.exports = global;
+
+
+//https://t.me/+5k-HpfOp-Aw1YzA8
+//join my telegram channel 
